01dc1631b432935aff480c6a81b36356e7e169fb
gugo <juanignacio.croce@gmail.com>
2023-10-20 11:37:15 -0400

    Formatted using https://github.com/chipsalliance/verible

--- cmod_s7_impl/test.srcs/sources_1/new/top.v
+++ cmod_s7_impl/test.srcs/sources_1/new/top.v
@@ -19,55 +19,54 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module Clock_divider(clock_in,clock_out
-    );
-input clock_in; // input clock on FPGA
-output reg clock_out; // output clock after dividing the input clock by divisor
-reg[27:0] counter=28'd0;
-parameter DIVISOR = 28'd2;
-// The frequency of the output clk_out
-//  = The frequency of the input clk_in divided by DIVISOR
-// For example: Fclk_in = 50Mhz, if you want to get 1Hz signal to blink LEDs
-// You will modify the DIVISOR parameter value to 28'd50.000.000
-// Then the frequency of the output clk_out = 50Mhz/50.000.000 = 1Hz
-always @(posedge clock_in)
-begin
- counter <= counter + 28'd1;
- if(counter>=(DIVISOR-1))
-  counter <= 28'd0;
+module Clock_divider (
+    clock_in,
+    clock_out
+);
+  input clock_in;  // input clock on FPGA
+  output reg clock_out;  // output clock after dividing the input clock by divisor
+  reg [27:0] counter = 28'd0;
+  parameter DIVISOR = 28'd2;
+  // The frequency of the output clk_out
+  //  = The frequency of the input clk_in divided by DIVISOR
+  // For example: Fclk_in = 50Mhz, if you want to get 1Hz signal to blink LEDs
+  // You will modify the DIVISOR parameter value to 28'd50.000.000
+  // Then the frequency of the output clk_out = 50Mhz/50.000.000 = 1Hz
+  always @(posedge clock_in) begin
+    counter <= counter + 28'd1;
+    if (counter >= (DIVISOR - 1)) counter <= 28'd0;
 
- clock_out <= (counter<DIVISOR/2)?1'b1:1'b0;
+    clock_out <= (counter < DIVISOR / 2) ? 1'b1 : 1'b0;
 
-end
+  end
 endmodule
 
-module top(
+module top (
     input clk,
     input [1:0] btn,
     output [3:0] led
-    );
-    
-    wire clock_out;
- // Instantiate the Unit Under Test (UUT)
- // Test the clock divider in Verilog
- Clock_divider
-    #(.DIVISOR(12_000))
-     uut 
-    (
-  .clock_in(clk), 
-  .clock_out(clock_out)
- );
-    
-    wire [7:0] myreg;
-    wire out_en;
-    
-    system m_system (
-        .clk (clock_out),
-        .resetn (!btn[0]),
-        //.trap (led[3]),
-        .out_byte (myreg),
-        .out_byte_en(out_en)
-        );
-    
-    assign led[3:0] = myreg[3:0];
+);
+
+  wire clock_out;
+  // Instantiate the Unit Under Test (UUT)
+  // Test the clock divider in Verilog
+  Clock_divider #(
+      .DIVISOR(12_000)
+  ) uut (
+      .clock_in (clk),
+      .clock_out(clock_out)
+  );
+
+  wire [7:0] myreg;
+  wire out_en;
+
+  system m_system (
+      .clk(clock_out),
+      .resetn(!btn[0]),
+      //.trap (led[3]),
+      .out_byte(myreg),
+      .out_byte_en(out_en)
+  );
+
+  assign led[3:0] = myreg[3:0];
 endmodule
