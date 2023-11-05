97688afdb89339202a51a811bd3ade2f96c12d7c
gugo <juanignacio.croce@gmail.com>
2023-11-05 22:17:12 +0000

    Current status of software, working but there is a bug when one letter is sent

--- software/firmware.c
+++ software/firmware.c
@@ -1,3 +1,6 @@
+#include <stdint.h>
+#include <stdbool.h>
+
 //volatile char * OUTPUT_REG = (volatile char *)0x10000000
 #define OUTPUT_REG (*((volatile char *)0x10000000))
 //volatile char * SERIAL_REG = (volatile char *)0x10000004;
@@ -19,6 +22,25 @@ void puts(const char *s)
 	while (*s) putc(*s++);
 }
 
+void putd(uint32_t val)
+{
+	char buffer[10];
+	char *p = buffer;
+	while (val || p == buffer) {
+		*(p++) = val % 10;
+		val = val / 10;
+	}
+	while (p != buffer) {
+		putc('0' + *(--p));
+	}
+}
+
+void puth(uint32_t val, uint32_t digits)
+{
+	for (int i = (4*digits)-4; i >= 0; i -= 4)
+		putc("0123456789ABCDEF"[(val >> i) % 16]);
+}
+
 void *memcpy(void *dest, const void *src, int n)
 {
 	while (n) {
@@ -26,6 +48,18 @@ void *memcpy(void *dest, const void *src, int n)
 		((char*)dest)[n] = ((char*)src)[n];
 	}
 	return dest;
+}
+
+void my_sleep_ms(unsigned int ms) {
+  #define STEPS_PER_SEC 1000
+  unsigned int i,s;
+  for (s=0; s < ms; s++) {
+    for (i=0; i < STEPS_PER_SEC; i++) {
+       // skip CPU cycle or any other statement(s) for making loop
+       // untouched by C compiler code optimizations
+       asm("nop");
+    }
+  }
 }
 
 void main()
@@ -53,4 +87,40 @@ void main()
 		}
 	puts(message);
 	puts("\r\n");
+
+	while(1) {
+		my_sleep_ms(10);
+		puts("Alive!\r\n");
+	}
+}
+
+uint32_t *irq(uint32_t *regs, uint32_t irqs)
+{
+	static unsigned int ext_irq_4_count = 0;
+	static unsigned int ext_irq_5_count = 0;
+	static unsigned int timer_irq_count = 0;
+
+	puts("Irq called: ");
+	puth(irqs, 8);
+	puts("\r\n");
+
+	if ((irqs & (1<<4)) != 0) {
+		ext_irq_4_count++;
+		puts("[EXT-IRQ-4] Serial: ");
+		putc(SERIAL_REG & 0xFF);
+	}
+
+	if ((irqs & (1<<5)) != 0) {
+		ext_irq_5_count++;
+		puts("[EXT-IRQ-5]");
+	}
+
+	if ((irqs & 1) != 0) {
+		timer_irq_count++;
+		puts("[TIMER-IRQ]");
+	}
+
+	puts("\r\n");
+
+	return regs;
 }
