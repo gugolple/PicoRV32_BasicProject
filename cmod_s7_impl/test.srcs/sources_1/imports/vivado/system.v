4194e86db5dcaf997df3a5e60d6c65b3361168da
juan <juanignacio.croce@gmail.com>
2023-11-07 17:40:02 +0100

    Reworked the system module. At some point adding logic to RAM logic made so that the compiler no longer solved it as RAM. Solved adding a new block that handled the remaining logic.

--- cmod_s7_impl/test.srcs/sources_1/imports/vivado/system.v
+++ cmod_s7_impl/test.srcs/sources_1/imports/vivado/system.v
@@ -26,7 +26,7 @@ module system (
 	wire [31:0] mem_addr;
 	wire [31:0] mem_wdata;
 	wire [3:0] mem_wstrb;
-	reg [31:0] mem_rdata;
+	wire [31:0] mem_rdata;
 
 	wire mem_la_read;
 	wire mem_la_write;
@@ -35,10 +35,7 @@ module system (
 	wire [3:0] mem_la_wstrb;
 
 	picorv32
-	   #( .ENABLE_IRQ(1),
-	   // Set stack address location, now 0 + MEM_SIZE -1
-	      .STACKADDR(MEM_SIZE * 32 / 4)
-       ) 
+	   #( .ENABLE_IRQ(1) ) 
 	   picorv32_core (
 		.clk         (clk         ),
 		.resetn      (resetn      ),
@@ -58,55 +55,75 @@ module system (
 		.irq         (irq),
 		.eoi         (eoi)
 	);
-    
+	
     // Request the memmory block
-	reg [31:0] memory [0:MEM_SIZE-1];
+	reg [31:0] memory [MEM_SIZE-1:0];
 	// LOAD IT FROM A HEX DIRECTLY, I LOVE THIS LINE!
 	initial $readmemh("firmware.hex", memory);
-
-	reg [31:0] m_read_data;
-	reg m_read_en;
-
+	
+    // This logic block defines a RAM behaviour, MUST be handled independently.
+    reg [31:0] mem_la_rdata_ram;
+    reg out_byte_en_ram;
+	
+	// Extra IO functionality
+	reg [31:0] mem_la_rdata_io;
+	reg out_byte_en_io;
+	
 	generate
 	   begin
-		always @(posedge clk) begin
-	       // ASYNC reset of the uart transmision for not retransmitting until
-	       // next write FROM cpu. This is only due to CPU SLOWER THAN UART.
-	        if (mem_instr) begin
-	           uart_dout_ctr <= 0;
-	        end
-		    
-		    // Normal memory behaviour when accessing RAM
-			mem_ready <= 1;
-			out_byte_en <= 0;
-			if ((mem_la_addr >> 2) < MEM_SIZE) begin
-                mem_rdata <= memory[mem_la_addr >> 2];
-                if (mem_la_write) begin
-                    if (mem_la_wstrb[0]) memory[mem_la_addr >> 2][ 7: 0] <= mem_la_wdata[ 7: 0];
-                    if (mem_la_wstrb[1]) memory[mem_la_addr >> 2][15: 8] <= mem_la_wdata[15: 8];
-                    if (mem_la_wstrb[2]) memory[mem_la_addr >> 2][23:16] <= mem_la_wdata[23:16];
-                    if (mem_la_wstrb[3]) memory[mem_la_addr >> 2][31:24] <= mem_la_wdata[31:24];
+	       // This awlays block DEFINES a RAM unit. DO NOT ADD TO IT.
+           always @(posedge clk) begin
+                // Normal memory behaviour when accessing RAM
+                mem_ready <= 1;
+                out_byte_en_ram <= 0;
+                if ((mem_la_addr >> 2) < MEM_SIZE) begin
+                    mem_la_rdata_ram <= memory[mem_la_addr >> 2];
+                    if (mem_la_write) begin
+                        if (mem_la_wstrb[0]) memory[mem_la_addr >> 2][ 7: 0] <= mem_la_wdata[ 7: 0];
+                        if (mem_la_wstrb[1]) memory[mem_la_addr >> 2][15: 8] <= mem_la_wdata[15: 8];
+                        if (mem_la_wstrb[2]) memory[mem_la_addr >> 2][23:16] <= mem_la_wdata[23:16];
+                        if (mem_la_wstrb[3]) memory[mem_la_addr >> 2][31:24] <= mem_la_wdata[31:24];
+                    end
+                end else begin
+                    mem_la_rdata_ram <= 32'b0;
                 end
             end
-            // Basic output register
-			if (mem_la_write && mem_la_addr == 32'h1000_0000) begin
-				out_byte_en <= 1;
-				out_byte <= mem_la_wdata;
-			end
-			// UART OUT!
-			if (mem_la_write && mem_la_addr == 32'h1000_0004) begin
-				if (mem_la_wstrb[0] && uart_dout_ctr == 0) begin
-				    uart_dout[ 7: 0] <= mem_la_wdata[ 7: 0];
-				    // Signal UART that data is valid
-				    uart_dout_ctr <= 1;
-				end
-			end
-			// UART IN!
-			if (!mem_la_write && mem_la_addr == 32'h1000_0004) begin
-			    mem_rdata[ 7: 0] <= uart_din[ 7: 0];
-				mem_rdata[ 8] <= uart_din_vld;
-				mem_rdata[ 9] <= uart_dout_rdy;
-			end
-		end
-	end endgenerate
+	
+            always @(posedge clk) begin
+                mem_ready <= 1;
+                // Basic output register
+                if (mem_la_write && mem_la_addr == 32'h1000_0000) begin
+                    out_byte_en_io <= 1;
+                    out_byte <= mem_la_wdata;
+                // UART REG!
+                end else if (mem_la_addr == 32'h1000_0004) begin
+                    // UART OUT!
+                    if (mem_la_write) begin
+                        if (mem_la_wstrb[0] && uart_dout_ctr == 0) begin
+                            uart_dout[ 7: 0] <= mem_la_wdata[ 7: 0];
+                            // Signal UART that data is valid
+                            uart_dout_ctr <= 1;
+                        end
+                    // UART IN!
+                    end else begin
+                        mem_la_rdata_io[ 7: 0] <= uart_din[ 7: 0];
+                        mem_la_rdata_io[ 8] <= uart_din_vld;
+                        mem_la_rdata_io[ 9] <= uart_dout_rdy;
+                    end
+                // Set to 0 when not in use to use OR to capture the values.
+                end else begin
+                    uart_dout_ctr <= 0;
+                    out_byte_en_io <= 0;
+                    mem_la_rdata_io <= 32'h00000000;
+                end
+                
+                // Concentrate OUT_BYTE_EN
+                out_byte_en <= out_byte_en_ram | out_byte_en_io;
+            end
+        end
+	endgenerate
+	
+	// This is a wire to remove 1 latch in the transmission.
+	// The latch is handled at the different sources.
+	assign mem_rdata = mem_la_rdata_ram | mem_la_rdata_io;
 endmodule
